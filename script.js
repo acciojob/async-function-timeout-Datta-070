bce03cce6e57ce899140d48d7c18dbf954dea834
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-01-03 13:08:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const submitButton = document.getElementById('btn');
+const outputDiv = document.getElementById('output');
+
+async function showMessageWithDelay(){
+	const text=textInput.value;
+	const delay=parseInt(delayInput.value, 10);
+}
+
+if(!text || isNaN(delay) || delay<=0){
+	outputDiv.innerText='Invalid Input';
+	return;
+}
+
+await new promise(resolve => setTimeOut(resolve, delay));
+
+outputDiv.innerText=text;
+
+submitButton.addEventListener('click', showMessageWithDelay);
