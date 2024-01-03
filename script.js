236ace2cc2ae9c3dbaa3315e29b516ee1fdbf524
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-01-03 13:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-//your JS code here. If required.
-const textInput = document.getElementById('text');
-const delayInput = document.getElementById('delay');
-const submitButton = document.getElementById('btn');
-const outputDiv = document.getElementById('output');
+btn.addEventListener('click', (e) => {
+	abc();
+})
 
-async function showMessageWithDelay(){
-	const text=textInput.value;
-	const delay=parseInt(delayInput.value, 10);
-
-if(!text || isNaN(delay) || delay<=0){
-	outputDiv.innerText='Invalid input';
-	return;
+async function abc() {
+	output.innerText = await new Promise((resolve, reject) => {
+		setTimeout(() => {
+			 resolve(text.value);
+		}, Number(delay.value));
+	})
 }
-
-await new promise(resolve => setTimeOut(resolve, delay));
-
-outputDiv.innerText=text;
-}
-
-submitButton.addEventListener('click', showMessageWithDelay);
